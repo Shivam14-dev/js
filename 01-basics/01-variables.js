d785b7dd37ea37ffe278d91f81cca81b2e0c9109
Shivam Suryawanshi <137019254+Shivam14-dev@users.noreply.github.com>
2024-01-22 11:13:51 +0000

    dataTypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "arnav@google.com"
+var accountPassword = "12345"
+accountCity = "dubai"
+let acccountState;
+//acccountId = 2
+
+
+accountEmail = "hv@hc.com"
+accountPassword = "12215481345"
+accountCity = "dubai"
+
+console.log(accountPassword);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, acccountState]);
+/*prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") // we are using node js not a Browser
+
+console.log(3+3) //code  readability extreme high
+
+console.log("shivam")
+
+
+let name ="shivam"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bignit
+// string =>""
+// bollen => true/false
+// null => standalome value (is a type of object)
+// undefined => value not assigned
+// symbol => unique
+
+
+
+//objects 
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);//object
